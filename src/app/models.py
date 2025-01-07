05efcb703de56353352434ac2342227ec79e1aae
“Ruslan <akhmarovri@yandex.ru>
2025-01-07 12:00:03 +0300

    Added created_at column

--- src/app/models.py
+++ src/app/models.py
@@ -1,8 +1,9 @@
+import datetime
 import decimal
 from typing import Optional
 
 from pydantic import BaseModel
-from sqlalchemy import Integer, Numeric, CheckConstraint, Text, BigInteger, Index
+from sqlalchemy import Integer, Numeric, CheckConstraint, Text, BigInteger, Index, DateTime, func
 from sqlalchemy.orm import declarative_base, Mapped, mapped_column
 
 
@@ -12,6 +13,7 @@ class PydWalletQueryBase(BaseModel):
 
 class PydWalletQuery(PydWalletQueryBase):
     query_id: Optional[int] = None
+    created_at: Optional[datetime.datetime] = datetime.datetime.now()
     balance: Optional[decimal.Decimal] = None
     bandwidth: Optional[int] = None
     energy: Optional[int] = None
@@ -24,6 +26,7 @@ class WalletQuery(Base):
     __tablename__ = "wallet"
 
     query_id: Mapped[Optional[int]] = mapped_column(primary_key=True, nullable=False)
+    created_at: Mapped[datetime.datetime] = mapped_column(DateTime(), default=func.now())
     address: Mapped[str] = mapped_column(Text())
     balance: Mapped[Optional[decimal.Decimal]] = mapped_column(Numeric())
     bandwidth: Mapped[Optional[int]] = mapped_column(Integer())
